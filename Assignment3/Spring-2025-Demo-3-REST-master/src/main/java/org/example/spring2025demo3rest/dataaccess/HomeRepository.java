4c86a61517e909368e37f5c831ab4d6a911af0ee
john-michael.woodrow <john-michael.woodrow19@ed.cna.nl.ca>
2025-03-31 14:02:54 -0230

    ass3

---
+++ Assignment3/Spring-2025-Demo-3-REST-master/src/main/java/org/example/spring2025demo3rest/dataaccess/HomeRepository.java
@@ -0,0 +1,34 @@
+package org.example.spring2025demo3rest.dataaccess;
+
+import org.example.spring2025demo3rest.pojos.Home;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+/**
+ * Home Repository interface that will be used by String to create a bean that handles all the CRUD operations
+ */
+public interface HomeRepository extends CrudRepository<Home, Long> {
+
+    /**
+     * Get all homes for a user
+     * @param userId
+     * @return
+     */
+    Iterable<Home> getAllByUserId(Long userId);
+
+    /**
+     * Get user by ID
+     * @param userId
+     * @return
+     */
+    Optional<Home> findByUserId(Long userId);
+
+
+    //Note: I am surprised this works! Spring detects it and fills in the method. Crazy.
+    //If this didn't work I would have retrieved all the homes and looped through and grab the homes that
+    //matched the user id.
+
+
+
+}
